6e054b09bc36c2904df9d138686a8259a504c8a3
Bruno Goniadis <brunogoniadislima@gmail.com>
2023-01-09 16:51:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import { Main } from './Main'
+
+function App() {
+
+  return (
+      <Main>
+        <h1>teste</h1>
+      </Main>
+  )
+}
+
+export default App
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+    min-width: 100vw;
+    min-height: 100vw;
+    
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    color: red;
+`
