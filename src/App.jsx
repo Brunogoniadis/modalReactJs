222c256e72f4faee1b95c12f57b50d09ce70ec1c
Bruno Goniadis <brunogoniadislima@gmail.com>
2023-01-12 18:38:59 -0300

    PrintScreen upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,23 @@ import { useState } from 'react'
 import { Modal } from './components/Modal'
 import { Main } from './Main'
 
-const AIRPLANE = 
-'https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80'
-
-const BEACH =
-'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-
-const CITY =
-'https://images.unsplash.com/photo-1444723121867-7a241cacace9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
 
 
 function App() {
 
   const [modalOpen, setModalOpen] = useState(false)
-  const [bg, setBG]= useState(`url(${AIRPLANE})`)
 
   return (
-      <Main style={{'--bg-image' : bg}}>
+      <Main >
+
 
         <Modal isOpen={modalOpen} setIsOpen={setModalOpen}>
-          <h2>Bruno Goniadis Lima</h2>
+          <h2>Content is Here</h2>
         </Modal>
 
-        <button onClick={()=> { setModalOpen(true) }}>Open Modal</button>
+        <button onClick={()=> { setModalOpen(true) }}>
+          Open Modal
+        </button>
    
       </Main>
   )
